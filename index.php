714cc6a07ac61c648be5a87bec9118bc6de8f833
Trevor97 <trevorsikaloka15@gmail.com>
2022-11-16 14:19:19 +0200

    style

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
     <title>Login | Rhino Leave Management System</title>
 </head>
 <body>
