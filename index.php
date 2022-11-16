b98dceca08b479c2aa7dad26d95f2b3f5eef308b
Trevor97 <trevorsikaloka15@gmail.com>
2022-11-16 15:10:30 +0200

    login form frontend

--- index.php
+++ index.php
@@ -4,11 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style/style.css">
     <title>Login | Rhino Leave Management System</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4">
+                <div class="card login-card">
+                    <div class="card-body">
+                        <h4 class="header-text text-center">Staff Login</h4>
+                        <p>Please login to continue</p>
+                        <form action="" method="post" class="form-group">
+                            <input type="email" name="email" id="email" class="form-control login-input" required>
+                            <input type="password" name="password" id="password" class="form-control login-input" required>
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-success login-input" type="button">Log In</button>
+                            </div>
+                        </form>
+                        <p class="sub-text">Don't have an account? <a href="">Request Now</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
